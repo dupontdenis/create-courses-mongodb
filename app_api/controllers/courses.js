8099bb98fe77b1955cc82632382c81ddbb967a22
Denis Dupont <denis.dupont@univ-evry.fr>
2022-11-21 11:09:33 +0100

    modif update one

--- app_api/controllers/courses.js
+++ app_api/controllers/courses.js
@@ -4,42 +4,21 @@ mongoose.connect('mongodb://localhost/my_courses', { useUnifiedTopology: true, u
 
 const debug = require('debug')('app_api');
 
-// const coursesReadAll = (req, res) => {
-//     res.json({ courses: courses });
-// };
 
 const coursesReadAll = async (req, res) => {
-    debug("API--- coursesReadAll ---");
+    debug("--- coursesReadAll ---");
     const courses = await Courses.find({});
     res.json({ courses });
 }
 
-// const coursesCreateOne = (req, res) => {
-//     debug("API--- coursesCreateOne ---");
-//     const course = {
-//         id: Math.ceil(Math.random() * 1000),
-//         name: req.body.name,
-//         info: req.body.info,
-//     }
-//     courses.push(course);
-//     res.json(course);
-// }
-
 const coursesCreateOne = async (req, res) => {
-    debug("API--- coursesCreateOne ---");
-    await Courses.create(req.body, (error, course) => {
-        res.json({ course });
-    })
+    debug("--- coursesCreateOne ---");
+    const course = await Courses.create(req.body)
+    res.json({ course });
 }
 
-// const coursesReadOne = (req, res) => {
-//     debug("API--- coursesReadOne ---");
-//     let course = find(courses, { 'id': Number(req.params.id) });
-//     if (!course) res.status(404).send(`The course with id:${req.params.id} was not found`)
-//     res.send(course);
-// }
 const coursesReadOne = async (req, res) => {
-    debug("API--- coursesReadOne ---");
+    debug("--- coursesReadOne ---");
     const course = await Courses.findById(req.params.id)
     if (!course) res.status(404).send(`The course with id:${req.params.id} was not found`)
     res.send(course);
@@ -47,24 +26,23 @@ const coursesReadOne = async (req, res) => {
 
 
 const coursesUpdateOne = async (req, res) => {
-    debug("API--- coursesUpdateOne ---");
-    Courses.findById(req.params.id, function (err, doc) {
-        if (err) debug("Houston we have a pb!")
+    debug("--- coursesUpdateOne ---");
+    try {
+        const doc = await Courses.findById(req.params.id)
         doc.name = req.body.name;
         doc.info = req.body.info;
         doc.save();
         res.send(doc);
-      });
+        
+    } catch (error) {
+        debug("Houston we have a pb!", error)
+    }
 }
 
-// const coursesDeleteOne = (req, res) => {
-//     debug("API--- coursesDeleteOne ---");
-//     pullAllBy(courses, [{ 'id': Number(req.params.id) }], 'id');
-//     res.json(courses);
-// }
+
 
 const coursesDeleteOne = async (req, res) => {
-    debug("API--- coursesDeleteOne ---");
+    debug("--- coursesDeleteOne ---");
     const course = await Courses.findByIdAndDelete(req.params.id)
     if (!course) res.status(404).send(`The course with id:${req.params.id} was not found`)
     res.send(null);
